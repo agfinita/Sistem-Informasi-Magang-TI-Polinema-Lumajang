eee5eacdd2b455a141f1203c51548b744d32dd14
raharjo <127064657+ariefraharjo@users.noreply.github.com>
2024-05-19 19:44:14 +0700

    edit admin

--- resources/views/pages/contents/admin/data-mahasiswa/create.blade.php
+++ resources/views/pages/contents/admin/data-mahasiswa/create.blade.php
@@ -232,11 +232,17 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js"></script>
     <script src="../assets/js/stisla.js"></script>
 
-    <!-- JS Libraies -->
+   <!-- JS Libraies -->
+   <script src="{{ asset('node_modules/simpleweather/jquery.simpleWeather.min.js') }}"></script>
+   <script src="{{ asset('node_modules/chart.js/dist/Chart.min.js') }}"></script>
+   <script src="{{ asset('node_modules/jqvmap/dist/jquery.vmap.min.js') }}"></script>
+   <script src="{{ asset('node_modules/jqvmap/dist/maps/jquery.vmap.world.js') }}"></script>
+   <script src="{{ asset('node_modules/summernote/dist/summernote-bs4.js') }}"></script>
+   <script src="{{ asset('node_modules/chocolat/dist/js/jquery.chocolat.min.js') }}"></script>
 
-    <!-- Template JS File -->
-    <script src="../assets/js/scripts.js"></script>
-    <script src="../assets/js/custom.js"></script>
+   <!-- Template JS File -->
+   <script src="{{ asset('assets/js/scripts.js') }}"></script>
+   <script src="{{ asset('assets/js/custom.js') }}"></script>
 
     <!-- Page Specific JS File -->
 </body>
