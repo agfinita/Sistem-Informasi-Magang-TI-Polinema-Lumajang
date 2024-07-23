aa5b2ed910f2b86847c223282a14084c7e45b1ca
raharjo <127064657+ariefraharjo@users.noreply.github.com>
2024-07-24 06:30:39 +0700

    ..

--- resources/views/pages/layouts/footer.blade.php
+++ resources/views/pages/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <!-- Footer -->
 <footer class="main-footer">
     <div class="footer-left">
-        Copyright &copy; 2024 Si-Mag
+        Copyright &copy; 2024 SI-Mag
         {{-- <div class="bullet"></div>
         Design By <a href="https://nauv.al/">Muhamad Nauval Azhar</a> --}}
     </div>
